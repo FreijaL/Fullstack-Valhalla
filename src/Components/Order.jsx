4410c5845fea5dcef21e9a870d7100e5a4ca5a9f
sebbanwalker <seb10walker@hotmail.com>
2023-11-29 07:48:31 +0100

    kitchen stuff done

--- src/Components/Order.jsx
+++ src/Components/Order.jsx
@@ -1,27 +1,79 @@
-// Order.js
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './Order.module.scss';
 
 const Order = ({ order }) => {
   const [isCompleted, setIsCompleted] = useState(false);
+  const initialTime = order.timeRemaining; // Initial time in seconds
+  const [elapsedTime, setElapsedTime] = useState(initialTime);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setElapsedTime(prevTime => prevTime + 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const getTimeColor = () => {
+    if (elapsedTime >= 1800) { // 30 minutes in seconds
+      return styles.timeRed;
+    } else if (elapsedTime >= 900) { // 15 minutes in seconds
+      return styles.timeYellow;
+    }
+    return '';
+  };
 
   const markAsCompleted = () => {
     setIsCompleted(true);
     // Additional logic to update the server state
   };
 
+  const getButtonStyle = () => {
+    if (elapsedTime < 60) { // First minute
+      return styles.startButton;
+    }
+    return styles.orderButton;
+  };
+
+  const getButtonText = () => {
+    if (elapsedTime < 60) { // First minute
+      return "Påbörja";
+    }
+    return "KLAR";
+  };
+
+  // Format the elapsed time as MM:SS
+  const formatTime = () => {
+    const seconds = elapsedTime % 60;
+    const minutes = Math.floor(elapsedTime / 60);
+
+    const formattedSeconds = seconds.toString().padStart(2, '0');
+    const formattedMinutes = minutes.toString().padStart(2, '0');
+
+    return `${formattedMinutes}:${formattedSeconds}`;
+  };
+
   return (
-    <div className={`${styles.orderCard} ${isCompleted ? styles.orderCardCompleted : ''}`}>
-      <div className={styles.orderCardHeader}>
-        <span>{order.id}</span>
-        <span>{order.timeRemaining}</span>
+    <div className={styles.orderCardContainer}>
+      <div className={`${styles.orderCard} ${isCompleted ? styles.orderCardCompleted : ''} ${getTimeColor()}`}>
+        <div className={styles.orderCardHeader}>
+          <span>{order.id}</span>
+          <span>{formatTime()}</span>
+        </div>
+        <div className={styles.orderCardDetails}>
+          <div className={styles.orderItems}>
+            {order.pizzas.map((pizza) => (
+              <div className={styles.itemContainer}  key={pizza.id}>
+                <p className={styles.itemTitle}>{pizza.name}</p>
+                <p className={styles.itemComment}>{pizza.comment}</p>
+              </div>
+          ))}
+          </div>
+          <p className={styles.commentTitle}>Kommentar från kund:</p>
+          <p className={styles.commentBread}>{order.comment}</p>
+        </div>
+        <button className={getButtonStyle()} onClick={markAsCompleted}>{getButtonText()}</button>
       </div>
-      <div className={styles.orderCardDetails}>
-        {order.pizzas.map((pizza) => (
-          <p key={pizza.id}>{pizza.name}</p>
-        ))}
-      </div>
-      <button onClick={markAsCompleted}>Klar</button>
     </div>
   );
 };
--- src/pages/staff/KitchenPage.jsx
+++ src/pages/staff/KitchenPage.jsx
@@ -6,34 +6,43 @@ import OrderList from '../../Components/OrderList';
 import Header from '../../Components/Header';
 
 function KitchenPage() {
+  const startTime = 0;
+
+  //For testing purposes
+  const startTime1 = 29 * 60 + 55;
+  const startTime2 = 14 * 60 + 54;
+  const startTime3 = 10 * 60;
+  const startTime4 = 5 * 60;
+  const startTime5 = 4 * 60 + 45;
+  const startTime6 = 52;
+
+
   // State management behövs
  //Fixar statisk lista sålänge
   const orders = [
     {
       id: '#58233',
-      timeRemaining: '30:45',
+      timeRemaining: startTime1,
       pizzas: [
-        { id: 'p1', name: 'Vesuvio x 1' },
-        { id: 'p2', name: 'Margherita x 1' },
-        { id: 'p3', name: 'Extra ost' },
+        { id: 'p1', name: 'Vesuvio x 1'},
+        { id: 'p2', name: 'Margherita x 1', comment: '+ Extra Ost' },
       ],
       isCompleted: false,
       comment: 'Lägg den extra osten bredvid tack!'
     },
     {
       id: '#58234',
-      timeRemaining: '15:45',
+      timeRemaining: startTime2,
       pizzas: [
-        { id: 'p4', name: 'Vesuvio x 1' },
+        { id: 'p4', name: 'Vesuvio x 1', comment: '- Ingen skinka' },
         { id: 'p5', name: 'Hawaii x 2' },
-        { id: 'p6', name: 'Ingen lök' },
       ],
       isCompleted: false,
       comment: 'Mindre lök än vanligt, tack.'
     },
     {
       id: '#58235',
-      timeRemaining: '10:10',
+      timeRemaining: startTime3,
       pizzas: [
         { id: 'p7', name: 'Capricciosa x 2' },
         { id: 'p8', name: 'Kebabpizza x 1' },
@@ -43,7 +52,7 @@ function KitchenPage() {
     },
     {
       id: '#58236',
-      timeRemaining: '5:00',
+      timeRemaining: startTime4,
       pizzas: [
         { id: 'p9', name: 'Quattro Stagioni x 1' },
         { id: 'p10', name: 'Calzone x 1' },
@@ -53,7 +62,7 @@ function KitchenPage() {
     },
     {
       id: '#58237',
-      timeRemaining: '45:30',
+      timeRemaining: startTime5,
       pizzas: [
         { id: 'p11', name: 'Funghi x 3' },
       ],
@@ -62,7 +71,7 @@ function KitchenPage() {
     },
     {
       id: '#58238',
-      timeRemaining: '25:15',
+      timeRemaining: startTime6,
       pizzas: [
         { id: 'p12', name: 'Pepperoni x 2' },
         { id: 'p13', name: 'Marinara x 1' },
