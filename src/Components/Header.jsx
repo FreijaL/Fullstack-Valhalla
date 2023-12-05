ff63954c7a3a06c280ca1d6870be2872089af0d7
Mercenaress <linden9707@gmail.com>
2023-12-05 12:10:04 +0100

    Added navigation to the drop menu.

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -11,13 +11,13 @@ function Header() {
             <header className={style.headerContainer}>
                 <img 
                     onClick={() => setShowMenu(prevstate => !prevstate)}
-                    className={style.headerMenuIcon} src="./svg/ham-menu.svg" alt="ham-menu-icon"
+                    className={style.headerMenuIcon} src="/svg/ham-menu.svg" alt="ham-menu-icon"
                 />
                 <section className={style.headerName}>
-                    <img className={style.headerNamePizzaIcon} src="./svg/pizza-slice.svg" alt="pizza-slice-logo" />
+                    <img className={style.headerNamePizzaIcon} src="/svg/pizza-slice.svg" alt="pizza-slice-logo" />
                     <h1 className={style.headerNameH1}>Valhalla Pizza</h1>
                 </section>
-                <img className={style.headerCartIcon} src="./svg/cart.svg" alt="cart-icon" />
+                <img className={style.headerCartIcon} src="/svg/cart.svg" alt="cart-icon" />
             </header>
             {
                 showMenu
