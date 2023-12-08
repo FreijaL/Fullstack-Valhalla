b772d56376ece8cbed562925a4a31d7f0df0b218
Mercenaress <linden9707@gmail.com>
2023-12-08 09:43:30 +0100

    Good comment goes here

--- src/pages/users/ConfirmationPage.jsx
+++ src/pages/users/ConfirmationPage.jsx
@@ -2,11 +2,16 @@ import style from './ConfirmationPage.module.scss';
 import Header from '../../Components/Header';
 import OrderConfirmationCard from '../../Components/OrderConfirmationCard';
 import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
 
 function ConfirmationPage() {
 
     const orderData = useSelector((state) => state.order);
     console.log("Conf page:", orderData);
+
+    useEffect(() => {
+        //Fetch order by ordernr
+    });
 
     return (
         <main className={style.confirmationPageContainer}>
